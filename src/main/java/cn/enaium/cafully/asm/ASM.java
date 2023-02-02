c37e8d7066a09ead44a6625a945e0167576fe55c
Enaium <wyy-666@hotmail.com>
2023-02-02 16:25:06 +0800

    first commit

---
+++ src/main/java/cn/enaium/cafully/asm/ASM.java
@@ -0,0 +1,16 @@
+package cn.enaium.cafully.asm;
+
+import cn.enaium.cafully.plugin.annotation.Plugin;
+import cn.enaium.cafully.plugin.api.IInitializer;
+import cn.enaium.cafully.plugin.helper.IHelper;
+
+/**
+ * @author Enaium
+ */
+@Plugin(unique = "asm", name = "ASM", version = "9.4", api = "*", description = "https://asm.ow2.io/")
+public class ASM implements IInitializer {
+    @Override
+    public void initialize(IHelper iHelper) throws Throwable {
+
+    }
+}
